55c3938282145f6869f025530cadffcf76961080
tianjuchen <chentianju@gmail.com>
2022-04-26 12:23:12 -0400

    add isokin ratio flow model

--- pyoptmat/optimize.py
+++ pyoptmat/optimize.py
@@ -39,7 +39,7 @@ import torch
 from torch.nn import Module, Parameter
 
 import pyro
-from pyro.nn import PyroModule, PyroSample
+from pyro.nn import PyroModule, PyroSample, PyroParam
 import pyro.distributions as dist
 from pyro.distributions import constraints
 from pyro import poutine
@@ -1125,5 +1125,237 @@ class HierarchicalNewScaleModel(PyroModule):
                         dist.Normal(results, full_noise),
                         obs=exp_results,
                     )
+
+        return results
+
+
+class HierarchicalMLE(PyroModule):
+    """
+    Wrap a material model to provide a hierarchical :py:mod:`pyro` statistical
+    model
+
+    This type of statistical model does two levels of sampling for each
+    parameter in the base model.
+
+    First it samples a random variable to select the mean and scale of the
+    population of parameter values
+
+    Then, based on this "top level" location and scale it samples each parameter
+    independently -- i.e. each experiment is drawn from a different "heat",
+    with population statistics given by the top samples
+
+    At least for the moment the population means are selected from
+    normal distributions, the population standard deviations from HalfNormal
+    distributions, and then each parameter population comes from a
+    Normal distribution
+
+    Args:
+      maker (function):                     function that returns a valid
+                                            :py:class`torch.nn.Module`, given the input
+                                            parameters
+      names (list(str)):                    names to use for the parameters
+      loc_loc_priors (list(tensor)):        location of the prior for the mean
+                                            of each parameter
+      loc_scale_priors (list(tensor)):      scale of the prior of the mean of each
+                                            parameter
+      scale_scale_priors (list(tensor)):    scale of the prior of the standard
+                                            deviation of each parameter
+      noise_priors (list or scalar):        random noise, can be either a single scalar
+                                            or a 1D tensor if it's a 1D tensor then each
+                                            entry i represents the noise in test type i
+
+    Keyword Args:
+      loc_suffix (str):                     append to the variable name to give the top
+                                            level sample for the location, default :code:`"_loc"`
+      scale_suffix (str):                   append to the variable name to give the top
+                                            level sample for the scale, default :code:`"_scale"`
+      param_suffix (str):                   append to the variable name to give the corresponding
+                                            :py:mod:`pyro.param` name, default
+                                            :code:`"_param"`
+      include_noise (str):                  if :code:`True` include white noise in the inference,
+                                            default :code:`False`
+    """
+
+    def __init__(
+        self,
+        maker,
+        names,
+        loc_loc_priors,
+        loc_scale_priors,
+        scale_scale_priors,
+        noise_prior,
+        loc_suffix="_loc",
+        scale_suffix="_scale",
+        param_suffix="_param",
+        include_noise=False,
+    ):
+        super().__init__()
+
+        # Store things we might later
+        self.maker = maker
+        self.loc_suffix = loc_suffix
+        self.scale_suffix = scale_suffix
+        self.param_suffix = param_suffix
+        self.include_noise = include_noise
+
+        self.names = names
+
+        # We need these for the shapes...
+        self.loc_loc_priors = loc_loc_priors
+        self.loc_scale_priors = loc_scale_priors
+        self.scale_scale_priors = scale_scale_priors
+        self.noise_prior = noise_prior
+
+        # What type of noise are we using
+        self.type_noise = noise_prior.dim() > 0
+
+        # Setup both the top and bottom level variables
+        self.bot_vars = names
+        self.top_vars = []
+        self.dims = []
+        for (
+            var,
+            loc_loc,
+            loc_scale,
+            scale_scale,
+        ) in zip(names, loc_loc_priors, loc_scale_priors, scale_scale_priors):
+            # These set standard PyroSamples with names of var + suffix
+            dim = loc_loc.dim()
+            self.dims.append(dim)
+            self.top_vars.append(var + loc_suffix)
+            setattr(
+                self,
+                self.top_vars[-1],
+                PyroParam(loc_loc, 
+                    constraint = constraints.interval(0.0, 1.0)),
+            )
+            self.top_vars.append(var + scale_suffix)
+            setattr(
+                self,
+                self.top_vars[-1],
+                PyroParam(scale_scale,
+                    constraint=constraints.positive),
+            )
+
+            # The tricks are: 1) use lambda self and 2) remember how python binds...
+            setattr(
+                self,
+                var,
+                PyroParam(torch.zeros_like(loc_loc), 
+                    constraint = constraints.interval(0.0, 1.0)
+                ),
+            )
+            
+        
+        # Setup the noise
+        if self.include_noise:
+            if self.type_noise:
+                self.eps = PyroParam(torch.ones_like(noise_prior), 
+                    constraint = constraints.positive)
+            else:
+                self.eps = PyroParam(torch.ones_like(noise_prior), 
+                    constraint = constraints.positive)
+        else:
+            self.eps = noise_prior
+
+        # This annoyance is required to make the adjoint solver work
+        # self.extra_param_names = []
+        self.extra_param_names = [var for var in self.names]
+
+    @property
+    def nparams(self):
+        """
+        Number of parameters in model
+        """
+        return len(self.names)
+
+    def sample_top(self):
+        """
+        Sample the top level variables
+        """
+        return [getattr(self, name) for name in self.top_vars]
+
+    def sample_bot(self):
+        """
+        Sample the bottom level variables
+        """
+        return [getattr(self, name) for name in self.bot_vars]
+
+    def make_guide(self):
+        # pylint: disable=unused-variable
+        """
+        Make the guide and cache the extra parameter names the adjoint solver
+        is going to need
+        """
+
+        def guide(exp_data, exp_cycles, exp_types, exp_control, exp_results=None):
+            # Setup and sample the top-level loc and scale
+            pass
+            
+        return guide
+
+    def get_extra_params(self):
+        """
+        Actually list the extra parameters required for the adjoint solve.
+
+        We can't determine this by introspection on the base model, so
+        it needs to be done here
+        """
+        # Do some consistency checking
+        for p in self.extra_param_names:
+            if p not in pyro.get_param_store().keys():
+                raise ValueError(f"Internal error, parameter {p} not in store!")
+
+        return [pyro.param(name).unconstrained() for name in self.extra_param_names]
+
+    def forward(self, exp_data, exp_cycles, exp_types, exp_control, exp_results=None):
+        # pylint: disable=unused-variable
+        """
+        Evaluate the forward model, optionally conditioned by the experimental
+        data.
+
+        Optionally condition on the actual data
+
+        See the :py:mod:`pyoptmat.experiments` module
+        for detailed on how to format the input to this function
+
+        Args:
+          exp_data (torch.tensor):    formatted input experimental data
+          exp_cycles (torch.tensor):  cycle counts for each test
+          exp_types (torch.tensor):   experiment types, as integers
+          exp_control (torch.tensor): stress/strain control flag
+
+        Keyword Args:
+          exp_results (torch.tensor): true results for conditioning
+        """
+        # Sample the top level parameters
+        curr = self.sample_top()
+        eps = self.eps
+
+        # Setup the full noise, which can be type specific
+        if self.type_noise:
+            full_noise = torch.empty(exp_data.shape[-1], device=exp_data.device)
+            for i in experiments.exp_map.values():
+                full_noise[exp_types == i] = eps[i]
+        else:
+            full_noise = eps
+
+        with pyro.plate("trials", exp_data.shape[2]):
+            # Sample the bottom level parameters
+            bmodel = self.maker(
+                *self.sample_bot(), extra_params=self.get_extra_params()
+            )
+            # Generate the results
+            predictions = bmodel.solve_both(
+                exp_data[0], exp_data[1], exp_data[2], exp_control
+            )
+            # Process the results
+            results = experiments.convert_results(
+                predictions[:, :, 0], exp_cycles, exp_types
+            )
+
+            # Sample!
+            with pyro.plate("time", exp_data.shape[1]):
+                pyro.sample("obs", dist.Normal(results, full_noise), obs=exp_results)
 
         return results
