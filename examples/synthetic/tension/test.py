173cf59cdd47c00f0aae1d958308d7b81a4ddc07
tianjuchen <chentianju@gmail.com>
2022-04-26 18:58:32 -0400

    replot synthetic data

---
+++ examples/synthetic/tension/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append('../../..')
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+
+    mpl.rcParams['png.fonttype'] = 42
--- examples/synthetic/tension/visualize_variance.py
+++ examples/synthetic/tension/visualize_variance.py
@@ -14,15 +14,17 @@ torch.set_default_tensor_type(torch.DoubleTensor)
 if __name__ == "__main__":
   scales = [0.0,0.01,0.05,0.1,0.15]
   
-  plt.style.use('single')
+  # plt.style.use('single')
   for scale in scales:
     data = xr.load_dataset("scale-%3.2f.nc" % scale)
 
     strain = data.strains.data[:,0]
     stress = data.stresses.data[:,0]
 
-    plt.plot(strain, stress)
-    plt.xlabel("Strain (mm/mm)")
-    plt.ylabel("Stress (MPa)")
+    plt.plot(strain, stress, lw = 2)
+    plt.xlabel("Strain (mm/mm)", fontsize=16)
+    plt.ylabel("Stress (MPa)", fontsize=16)
+    plt.xticks(fontsize=11)
+    plt.yticks(fontsize=11)
     plt.tight_layout()
-    plt.savefig("visualize-%3.2f.pdf" % scale)
+    plt.savefig("visualize-%3.2f.pdf" % scale, dpi=600)
