dfd93be7643ef963ce0c385d355d96967c23b3cb
tianjuchen <chentianju@gmail.com>
2023-07-11 22:49:31 -0400

    adding the partial flow rule

--- test/test_flowrules.py
+++ test/test_flowrules.py
@@ -1,3 +1,6 @@
+import sys
+
+sys.path.append("..")
 import unittest
 
 import numpy as np
@@ -488,6 +491,77 @@ class TestIsoKinChabocheViscoplasticity(unittest.TestCase, CommonFlowRule):
 
         self.model = flowrules.IsoKinViscoplasticity(
             CP(self.n), CP(self.eta), CP(self.s0), self.iso, self.kin
+        )
+
+        self.s = torch.linspace(150, 200, self.nbatch)
+        self.h = torch.reshape(
+            torch.tensor(
+                np.array(
+                    [
+                        np.linspace(51, 110, self.nbatch),
+                        np.linspace(-10, 21, self.nbatch)[::-1],
+                        np.linspace(0, 2, self.nbatch),
+                        np.linspace(-2, 0, self.nbatch),
+                    ]
+                )
+            ).T,
+            (self.nbatch, 4),
+        )
+        self.t = torch.ones(self.nbatch)
+        self.T = torch.zeros_like(self.t)
+        self.erate = torch.linspace(1e-2, 1e-3, self.nbatch)
+
+        self.skip = False
+
+    def test_kin(self):
+        def fn(i):
+            hp = self.h.clone()
+            hp[:, 1] = i
+            return self.model.flow_rate(self.s, hp, self.t, self.T, self.erate)[0]
+
+        i1 = utility.differentiate(fn, self.h[:, 1])
+        i2 = self.model.dflow_dkin(self.s, self.h, self.t, self.T, self.erate)
+        self.assertTrue(np.allclose(i1, i2, rtol=1.0e-4))
+
+    def test_iso(self):
+        def fn(i):
+            hp = self.h.clone()
+            hp[:, 0] = i
+            return self.model.flow_rate(self.s, hp, self.t, self.T, self.erate)[0]
+
+        i1 = utility.differentiate(fn, self.h[:, 0])
+        i2 = self.model.dflow_diso(self.s, self.h, self.t, self.T, self.erate)
+
+        self.assertTrue(np.allclose(i1, i2, rtol=1.0e-4))
+
+class TestIsoKinPartialViscoplasticity(unittest.TestCase, CommonFlowRule):
+    def setUp(self):
+        self.n = torch.tensor(5.2)
+        self.eta = torch.tensor(110.0)
+        self.s0 = torch.tensor(11.0)
+        self.alpha = torch.tensor(0.1)
+        self.beta = torch.tensor(0.9)
+        self.gamma = torch.tensor(0.5)
+
+        self.nbatch = 10
+
+        self.R = torch.tensor(101.0)
+        self.d = torch.tensor(1.3)
+        self.iso = hardening.VoceIsotropicHardeningModel(CP(self.R), CP(self.d))
+
+        self.C = torch.tensor([100.0, 1000, 1500])
+        self.g = torch.tensor([1.2, 100, 50])
+        self.kin = hardening.ChabocheHardeningModel(CP(self.C), CP(self.g))
+
+        self.model = flowrules.IsoKinPartialViscoplasticity(
+            CP(self.n),
+            CP(self.eta),
+            CP(self.s0),
+            CP(self.alpha),
+            CP(self.beta),
+            CP(self.gamma),
+            self.iso,
+            self.kin,
         )
 
         self.s = torch.linspace(150, 200, self.nbatch)
